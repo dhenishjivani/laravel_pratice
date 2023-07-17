a17ce445dae99aeea206bf33c09916e90d2407b0
dhenish <dhenish@simformsolutions.com>
2023-07-17 09:37:42 +0530

    Queue Jobs with Dispatch method and handle method

--- app/Http/Controllers/SendOrderMailController.php
+++ app/Http/Controllers/SendOrderMailController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\SendTestMailJob;
 use App\Mail\OrderShipped;
 use Illuminate\Http\Request;
 use App\Models\User;
@@ -10,8 +11,17 @@ use Illuminate\Support\Facades\Mail;
 class SendOrderMailController extends Controller
 {
     //
-    public function SendMail() {
-        $user = User::where('id', 1)->first();
-        Mail::to($user)->send(new OrderShipped($user));
+    public function SendMail()
+    {
+        // Clouser
+        // dispatch(function () {
+        //     $user = User::where('id', 1)->first();
+        //     Mail::to($user)->send(new OrderShipped($user));
+        // })->delay(now()->addMinute());
+
+
+        // Aa bane madhi pan koy aek use kari saki as you wish
+        // dispatch(new SendTestMailJob())->delay(now()->addMinute());
+        // \App\Jobs\SendTestMailJob::dispatch()->delay(now()->addMinute());
     }
 }
---
+++ app/Jobs/SendTestMailJob.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\User;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\OrderShipped;
+
+class SendTestMailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        //
+        $user = User::where('id', 1)->first();
+        Mail::to($user)->send(new OrderShipped($user));
+    }
+}
